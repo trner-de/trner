f94e5a57267e0b6b814229330e0d9b9761c459e9
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-09-17 05:17:06 +0000

    added tailwind

--- apps/trner-de/src/app/app.module.ts
+++ apps/trner-de/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
+import { UiModule } from '@trner/ui';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    UiModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
