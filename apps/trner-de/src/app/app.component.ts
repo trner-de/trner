1e291f5bfc65ade12fcff82574ee79041711d54f
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-09-28 12:03:59 +0000

    added class theme

--- apps/trner-de/src/app/app.component.ts
+++ apps/trner-de/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,8 +7,14 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'trner-de';
 
+	constructor(@Inject(DOCUMENT) private document: Document) { }
+
+	ngOnInit(): void {
+		console.log();
+		this.document.documentElement.classList.add('dark')
+	}
 }
 
